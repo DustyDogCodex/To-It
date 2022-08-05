9a9d456e34d595861ba88d714932d0eac455c9be
Varun Malaviya <varunmalaviya7@gmail.com>
2022-08-04 20:19:19 -0500

    clearing out HTML/CSS code for now. I don't like this design anymore so I'm restarting it from scratch.

--- src/addToDoModal.js
+++ src/addToDoModal.js
@@ -4,7 +4,7 @@ class NewToDo {
         const newToDo = document.createElement('form')
         
         newToDo.classList.add('newToDoModal')
-        newToDo.innerHTML = '<label>To-Do: <input type=\'text\' name=\'toDo\'></label> <label>Due Date: <input type=\'date\' name=\'dueDate\'></label> <label>Priority: <input type=\'radio\' name=\'low\'><input type=\'radio\' name=\'medium\'><input type=\'radio\' name=\'high\'></label> <input type=\'submit\'>'
+        /* newToDo.innerHTML = '<label>To-Do: <input type=\'text\' name=\'toDo\'></label> <label>Due Date: <input type=\'date\' name=\'dueDate\'></label> <label>Priority: <input type=\'radio\' name=\'low\'><input type=\'radio\' name=\'medium\'><input type=\'radio\' name=\'high\'></label> <input type=\'submit\'>' */
 
         content.appendChild(newToDo)
     }
